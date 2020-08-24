cf83f8e1fcbe978afa26f95b1f3666f8bbb84b3e
gaubian <guillaume.aubian@gaubian.xyz>
2020-08-24 19:00:05 +0000

    Added blog post on smash tier list

--- bootstrap.py
+++ bootstrap.py
@@ -3,7 +3,7 @@ import datetime as dt
 import time
 
 def erase_site():
-    os.system('rm -rf site/*')
+    os.system('rm -rf site/*.html')
 
 def pandoc(x):
     s = 'pandoc -B includes/head -A includes/foot -o site/' + x[:-3] + '.html src/' + x
